ad594da29214a86699d407af348abb98ab242ec4
nvld <menoret.quentin@gmail.com>
2017-01-23 22:52:16 +0100

    Solution for grains

---
+++ grains/src/Grains.hs
@@ -0,0 +1,12 @@
+module Grains (square, total) where
+
+import Data.Maybe
+
+square :: Integer -> Maybe Integer
+square x | x < 1 || x > 64 = Nothing
+square n = Just (2^(n-1))
+
+total :: Integer
+total = ((* 2) . fromJust . square) 64 - 1
+-- total = foldl (\acc x -> ((acc +) . fromJust . square) x) 0 [1..64]
+
