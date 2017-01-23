e1d918997b4b7224f4c57d253241c785cd9ed6de
nvld <menoret.quentin@gmail.com>
2017-01-23 19:10:59 +0100

    Second iteration for run-length-encoding

--- run-length-encoding/src/RunLength.hs
+++ run-length-encoding/src/RunLength.hs
@@ -1,49 +1,20 @@
 module RunLength (decode, encode) where
 
 import Data.Char
-import Data.Maybe
 
-
--- Helper type
-type Count  = Int
-type Occ    = (Count, Maybe Char)
-
-nbOcc :: Occ -> Count
-nbOcc (x, _) = x
-
-charOcc :: Occ -> Maybe Char
-charOcc (_, y) = y
-
-realChar :: Occ -> Char
-realChar = fromJust . charOcc
-
--- Encoding part
-split :: [Occ] -> String -> [Occ]
-split xs "" = xs
-split (x:xs) (y:ys) | y == realChar x = split ((1 + nbOcc x, Just y):xs) ys
-split xs     (y:ys)                   = split ((1, Just y):xs) ys
-
-encodeOcc :: [Occ] -> String
-encodeOcc [] = ""
-encodeOcc (x:xs) = encodeOcc xs ++ count ++ [realChar x]
-    where count = if nbOcc x == 1 then "" else (show . nbOcc) x
-
--- Decoding part
-splitEncoded :: [Occ] -> String -> [Occ]
-splitEncoded ys "" = ys
-splitEncoded (y:ys) (x:xs) | isDigit x && (isNothing . charOcc) y = splitEncoded ((10 * nbOcc y + digitToInt x, Nothing):ys) xs
-splitEncoded (y:ys) (x:xs) | isNothing (charOcc y)                = splitEncoded ((nbOcc y, Just x):ys) xs
-splitEncoded ys     (x:xs) | isDigit x                            = splitEncoded ((digitToInt x, Nothing):ys) xs
-splitEncoded ys     (x:xs)                                        = splitEncoded ((1, Just x):ys) xs
-
-decodeOcc :: [Occ] -> String
-decodeOcc [] = ""
-decodeOcc (x:xs) = decodeOcc xs ++ replicate (nbOcc x) (realChar x)
-
--- Exports
 decode :: String -> String
-decode = decodeOcc . splitEncoded []
+decode "" = ""
+decode xs = replicate nb ch ++ (decode . drop 1 . snd) ss
+    where ss = span isDigit xs
+          nb = max 1 $ (read . ('0':) . fst) ss
+          ch = (head . snd) ss
 
 encode :: String -> String
-encode = encodeOcc . split []
+encode "" = ""
+encode xs = dispCount ++ char : encode ys
+    where ss = span (\x -> x == head xs) xs
+          ys = snd ss
+          count = (length . fst) ss
+          dispCount = if count == 1 then "" else show count
+          char = head xs
 
