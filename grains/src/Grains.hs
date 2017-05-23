12f394e1663defac3f81fa298bf0214e1c4929a4
nvld <menoret.quentin@gmail.com>
2017-05-24 00:44:36 +0200

    Add another solution for grains

--- grains/src/Grains.hs
+++ grains/src/Grains.hs
@@ -7,6 +7,7 @@ square x | x < 1 || x > 64 = Nothing
 square n = Just (2^(n-1))
 
 total :: Integer
-total = ((* 2) . fromJust . square) 64 - 1
+total = (sum . mapMaybe square) [1..64]
+-- total = ((* 2) . fromJust . square) 64 - 1
 -- total = foldl (\acc x -> ((acc +) . fromJust . square) x) 0 [1..64]
 
